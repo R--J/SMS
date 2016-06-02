fdded68a15fd9e4d2f81891ea3a88b84f10bc3f2
R--J <431954839@qq.com>
2016-06-02 18:00:12 +0800

    update __init__.py

--- sms/__init__.py
+++ sms/__init__.py
@@ -1,12 +1,8 @@
 # Import flask and template operators
 from flask import Flask, render_template, g
+
 # Define the WSGI application object
 app = Flask(__name__, static_folder='resources', template_folder='views')
-
-# Import SQL
-# from flask.ext.sqlalchemy import SQLAlchemy
-# Define the database object which is imported by modules and controllers
-# db = SQLAlchemy(app)
 
 # Configurations
 app.config.from_object('config')
@@ -17,16 +13,15 @@ app.config.from_object('config')
 def not_found(error):
     return render_template('404.html'), 404
 
-
 # Import modules
 from sms.controllers.auth import auth
 modules = [
     auth,
 ]
+
 # Add modules to app
 for module in modules:
     app.register_blueprint(auth)
-
 
 # Database
 from sms.repository.handler import get_db
